aa4ca3e8b061a5b69dcb3b78f67979559c870514
Kevin-S <kevsilvo@gmail.com>
2019-05-09 19:06:48 -0400

    listener error

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -9,13 +9,14 @@ class TabLink {
     this.data = element.dataset.tab;
 
     // Using the custom data attribute get the associated Item element
-    this.itemElement = document.querySelector(`.tabs-item[data-tab=${this.data}]`);
+    this.itemElement = document.querySelector(`.tabs-item[data-tab='${this.data}']`);
     
     // Using the Item element, create a new instance of the TabItem class
-     this.tabItem = new TabItem(this.tabItem);
+     this.itemElement = new TabItem(this.itemElement);
+     console.log(this.itemElement);
     
     // Add a click event listener on this instance, calling the select method on click
-    this.tabItem.addEventListener('click', () => {
+    this.itemElement.addEventListener('click', () => {
       this.select();
     })
 
@@ -23,15 +24,18 @@ class TabLink {
 
   select() {
     // Get all of the elements with the tabs-link class
-    // const links;
+       const links = document.querySelectorAll('.tabs-link');
 
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
-    // Array.from(links).forEach();
+     Array.from(links).forEach(function(element){
+        element.classList.remove('tabs-link-selected');
+     });
 
     // Add a class named "tabs-link-selected" to this link
-    // this.element;
+     this.element.classList.add('tabs-link-selected');
     
     // Call the select method on the item associated with this link
+    this.element.select();
 
   }
 }
